f62783c4ba14c9267389df3a3d4f05ec6677f1ff
gangadharamurthy <46064649+gangadharamurthy@users.noreply.github.com>
2018-12-22 20:27:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <p> This is my aws bean stalk paas</p>
   <p1> hey boss code is ready</p1>
   <p> shankar direction new movie is idian 2  </p>
-  <p> AWS cloud for Gangadhar , Water found in Moon space testing </p>
+  <p> AWS cloud for Gangadhar , Water found in Moon space testing123 </p>
 
 <?php
 echo "Version: 5.2";
